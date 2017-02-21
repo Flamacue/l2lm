304db618e8622dd5ad3c1fd6604b100a75980c2d
Adam K Sumner <asumner101@gmail.com>
2017-02-20 20:40:02 -0500

    add loading screen when fetching information

--- client/app/components/event/event.controller.js
+++ client/app/components/event/event.controller.js
@@ -6,14 +6,18 @@ function EventController(EventService, ArtistService){
   vm.artist = ArtistService.getArtist;
   vm.events = [];
   vm.getEvents = getEvents;
+  vm.loading = false;
 
   function getEvents(){
+    vm.loading = true;
     EventService.getArtistEvents(vm.artist())
       .then(
         function(response){
+          vm.loading = false;
           vm.events = response.data.results.events;
         },
         function(response){
+          vm.loading = false;
           vm.events = "Something went wrong";
         }
       );
--- client/app/components/track/track.controller.js
+++ client/app/components/track/track.controller.js
@@ -4,17 +4,20 @@ angular.module('app')
 function TrackController(TrackService, ArtistService){
   var vm = this;
   vm.artist = ArtistService.getArtist;
+  vm.loading = false;
   vm.topTracks = [];
   vm.getTopTracks = getTopTracks;
 
   function getTopTracks(){
+    vm.loading = true;
     TrackService.getTopTracks(vm.artist())
       .then(
         function(response){
+          vm.loading = false;
           vm.topTracks = response.data.results.tracks;
-          console.log(vm.topTracks)
         },
         function(response){
+          vm.loading = false;
           vm.topTracks = "Something went wrong";
         });;
   }
